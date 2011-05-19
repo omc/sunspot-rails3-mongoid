6378488ad4b5be2862353bf1a34a579504dec25c
Nick Zadrozny <nick@beyondthepath.com>
2011-05-19 08:11:36 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,15 @@
+# Sunspot sample app
+
+## Rails 3 + Mongoid
+
+This app exists for a few reasons...
+
+1. Provide an example of how to use Sunspot.
+2. Provide integration testing for Sunspot.
+
+### Pull requests welcome
+
+Do you have a pet feature that your app uses? Model it out in here so we can all benefit from a collection of best practices.
+
+Found a bug in Sunspot? Fork this application, duplicate the bug, and send us a pull request. Anything goes!
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SunspotRails3Mongoid::Application.config.secret_token = '17214181dcb32b2a40251bac0e75502bd169910902c0f3ce1023b60224e9971524f4e935836cb9e0b22f4b0dc82682bb3249270d141bc85272ad3f7038683360'
